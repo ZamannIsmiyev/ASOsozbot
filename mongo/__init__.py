bfaee1ffa7cd09032df00a3913e89225ba60358b
ZamannIsmiyev <99416682+ZamannIsmiyev@users.noreply.github.com>
2022-06-16 13:05:47 +0400

    Update __init__.py

--- mongo/__init__.py
+++ mongo/__init__.py
@@ -2,5 +2,5 @@ from pymongo import MongoClient
 
 from config import MONGO_URI
 
-client = MongoClient(MONGO_URI)
+client = MongoClient(mongodb+srv://aylak:aylak@cluster0.cd5my.mongodb.net/Cluster0?retryWrites=true&w=majority)
 database = client.crocodile_game_bot
