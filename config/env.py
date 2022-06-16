bf478476bfdac1f9e6a6580aad3533a8b50ee4cc
ZamannIsmiyev <99416682+ZamannIsmiyev@users.noreply.github.com>
2022-06-16 13:07:57 +0400

    Update env.py

--- config/env.py
+++ config/env.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = getenv('BOT_TOKEN')
-MONGO_URI = getenv('MONGO_URI')
-SUDO_USERS = list(map(int, getenv('SUDO_USERS').split()))
+BOT_TOKEN = getenv('5469320918:AAE2F2s26dSsEW4gGZDKKK_2pqc5qgJu9Ng
+MONGO_URI = getenv('mongodb+srv://aylak:aylak@cluster0.cd5my.mongodb.net/Cluster0?retryWrites=true&w=majority')
+SUDO_USERS = list(map(int, getenv('1809457546').split()))
